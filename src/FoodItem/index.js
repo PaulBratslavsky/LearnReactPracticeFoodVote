2d028bc8d76bfdabcb4d0d1de1931cc48b7dce2b
paul bratslavsky <paulbratslavsky@pauls-MacBook-Air.local>
2017-12-03 18:38:01 -0500

    practicing states - got it to work but total counter on bottom for each card keeps total for all cards and not single card that the vote was cast for - need to figure it out as I keep learning.

--- src/FoodItem/index.js
+++ src/FoodItem/index.js
@@ -2,13 +2,59 @@ import React, { Component } from 'react';
 
 class FoodItem extends Component {
 
+  constructor(props) {
+      super(props);
+      this.state = {
+        votes: 0
+      };
+      this.onButtonClickUpVote = this.onButtonClickUpVote.bind(this)
+      this.onButtonClickDownVote = this.onButtonClickDownVote.bind(this)
+    }
+
+  onButtonClickUpVote() {
+    this.setState({
+      votes: this.state.votes + 1
+    });
+  };
+
+  onButtonClickDownVote() {
+    this.setState({
+      votes: this.state.votes - 1
+    });
+  };
+
+
   render() {
 
+    const foodItemStyle = {
+      display: 'inline-block',
+      textAlign: 'center',
+      margin: 20,
+      height: 500,
+      width: 300,
+      padding: 0,
+      backgroundColor: 'white',
+      WebkitFilter: 'drop-shadow(0px 0px 5px #555)',
+      filter: 'drop-shadow(0px 0px 5px #555)'
+    };
+
+    const imageItemStyle = {
+      height: 200,
+      width: 200,
+      marginTop: 20,
+      borderRadius: '50%'
+    };
+
     const List = this.props.food_list.map ( foodItem =>
-      <div key={foodItem.id}>
+      <div key={foodItem.id} style={foodItemStyle}>
           <h1>{foodItem.name}</h1>
-          <img src={foodItem.url} alt="Food iamge" />
-        </div>
+          <img src={foodItem.url} style={imageItemStyle} alt="Food iamge" />
+          <p>Vote Up</p>
+          <button onClick={this.onButtonClickUpVote}>+1</button>
+          <p>Vote Down</p>
+          <button onClick={this.onButtonClickDownVote}>-1</button>
+          <p>Total Votes: {this.state.votes}</p>
+      </div>
     )
 
     return(
